35e1789f79a35864ebb65d88579651d8f5542378
Soions <avtar7481@gmail.com>
2024-12-19 21:17:54 +0300

    greeting.py

--- greeting.py
+++ greeting.py
@@ -1,13 +1,13 @@
 username = 'Алексей'
 title = 'Купить вещи для похода'
-content = 'Платака, спальный мешок, походная кухня.'
+content = 'Палатка, спальный мешок, походная кухня.'
 status = 'Активна'
 created_date = '14.12.2024'
 issue_date ='17.12.2024'
 
-print('Имя пользователя:', username)
-print('Заголовок:', title)
-print('Описание заметки:', content)
-print('Статус заметки:', status)
-print('Дата создания:', created_date)
-print('Дата окончания:', issue_date)
+print('Имя пользователя: ', username)
+print('Заголовок: ', title)
+print('Описание заметки: ', content)
+print('Статус заметки: ', status)
+print('Дата создания: ', created_date)
+print('Дата окончания: ', issue_date)
