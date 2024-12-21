f56c38ec182f4f415422e141aff6fb76fbdf266a
Soions <avtar7481@gmail.com>
2024-12-21 23:47:15 +0300

    Update update_status.py

--- update_status.py
+++ update_status.py
@@ -1,11 +1,10 @@
-status = ["Выполнено", "В процессе", "Отложено"]
-print('Статусы заметки: \nВыполнено: 1 \nВ процессе: 2 \nОтложено: 3')
+change_status = ["Выполнено", "В процессе", "Отложено"]
+print('Текущий статус заметки: В процессе')
+print('Выберити новый статус заметки: \n1: Выполнено  \n2: В процессе \n3: Отложено')
 note = input('\nВведите статус заметки: ')
 if note == '1':
-    print(status[0])
+    print(change_status[0])
 elif note == '2':
-    print(status[1])
+    print(change_status[1])
 elif note == '3':
-    print(status[2])
-
-
+    print(change_status[2])
