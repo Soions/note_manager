796b3d02e864ca7b3757b9e66dfde8ac20957b66
Soions <avtar7481@gmail.com>
2025-01-11 10:45:01 +0300

    Update update_status.py

--- update_status.py
+++ update_status.py
@@ -1,10 +1,67 @@
-change_status = ["Выполнено", "В процессе", "Отложено"]
-print('Текущий статус заметки: В процессе')
-print('Выберити новый статус заметки: \n1: Выполнено  \n2: В процессе \n3: Отложено')
-note = input('\nВведите статус заметки: ')
-if note == '1':
-    print(change_status[0])
-elif note == '2':
-    print(change_status[1])
-elif note == '3':
-    print(change_status[2])
+def create_note():
+    notes = []
+    username = input("Введите имя пользователя: ")
+    title = input("Введите заголовок заметки: ")
+    content = input("Введите описание заметки: ")
+    status = input("Введите статус заметки (новая, в процессе, выполнено): ")
+    created_date = input("Введите дату создания (день-месяц-год): ")
+    issue_date = input("Введите дедлайн (день-месяц-год): ")
+
+    new_note = {
+        "Имя": username,
+        "Заголовок": title,
+        "Описание": content,
+        "Статус": status,
+        "Дата создания": created_date,
+        "Дедлайн": issue_date
+    }
+
+    notes.append(new_note)
+
+    return notes
+
+def print_note():
+    print("Список заметок:")
+    for i, note in enumerate(notes_list, 1):
+        print(f"{i}. Имя: {note['Имя']}")
+        print(f"  Заголовок: {note['Заголовок']}")
+        print(f"  Описание: {note['Описание']}")
+        print(f"  Статус: {note['Статус']}")
+        print(f"  Дата создания: {note['Дата создания']}")
+        print(f"  Дедлайн: {note['Дедлайн']}")
+
+
+notes_list = []
+while True:
+    print("Введите данные заметок: ")
+
+    new_notes = create_note()
+    notes_list.extend(new_notes)
+
+    print("Хотите добавить ещё одну заметку? (да/нет): ", end="")
+    response = input().lower()
+
+    if response == "нет":
+        break
+print_note()
+
+while True:
+    print("Хотите изменить статус заметки (да/нет): ", end="")
+    response = input().lower()
+
+    if response == "нет":
+        break
+
+    print("Введите имя заголовка: ", end="")
+    criterion = input()
+
+    title_notes = [note for note in notes_list if note['Заголовок'] == criterion]
+    if title_notes:
+        print('Введите новый статус заметки: ', end='')
+        new_status = input()
+        notes_list[notes_list.index(title_notes[0])]["Статус"] = new_status
+        print(f"Статус заметки с заголовком {criterion} успешно изменен на {new_status}.")
+
+    else:
+        print("Заметок, соответствующих критерию, не найдено.")
+print_note()
